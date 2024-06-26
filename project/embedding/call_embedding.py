93c1d2251009d19133fc4bb75762d0af9d7b9dd5
junmry <89402015+junmry@users.noreply.github.com>
2024-06-26 10:13:03 +0800

    Delete project directory

--- project/embedding/call_embedding.py
+++
@@ -1,8 +0,0 @@
-from embedding.Custom_LangChain_Embeddings import CustomLangChainEmbeddings
-
-def get_embedding(embedding: str):
-
-    if embedding == 'm3e':
-        return  CustomLangChainEmbeddings()
-    else:
-        raise ValueError(f"embedding {embedding} not support ")
